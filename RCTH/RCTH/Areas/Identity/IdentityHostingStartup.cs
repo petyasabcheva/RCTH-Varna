d415bb9d2e120f2991d154c6f8b576f2ef3fa81b
Martin <mirander73@gmail.com>
2021-03-26 21:46:40 +0200

    initial

---
+++ RCTH/RCTH/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using RCTH.Data;
+
+[assembly: HostingStartup(typeof(RCTH.Areas.Identity.IdentityHostingStartup))]
+namespace RCTH.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}

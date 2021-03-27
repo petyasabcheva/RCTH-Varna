66d7977bcdb8d37c7dc58ee231be037223a4074e
Martin <mirander73@gmail.com>
2021-03-27 21:36:55 +0200

    blood ratio calculations implemented

---
+++ src/RCTH/Controllers/MedResultController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RCTH.Controllers
+{
+    public class MedResultController : Controller
+    {
+        public IActionResult MedResult()
+        {
+            return View();
+        }
+
+
+    }
+}
